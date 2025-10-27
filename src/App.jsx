d2b6928684c2c56b3bb33cad7751d9dba7951230
Lakshay Mittal <lakshayjain0611@gmail.com>
2025-10-28 04:10:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+import ComplaintDashboard from './component/dashboard'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <ComplaintDashboard />
+    </>
+  )
+}
+
+export default App
